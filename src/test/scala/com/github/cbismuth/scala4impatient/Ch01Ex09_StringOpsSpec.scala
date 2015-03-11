6e68c5a68ef1dd51b8d957cf98653186a72ccb32
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-11 12:15:35 +0100

    chapter 01 exercise 09 - string ops head/last

---
+++ src/test/scala/com/github/cbismuth/scala4impatient/Ch01Ex09_StringOpsSpec.scala
@@ -0,0 +1,13 @@
+package com.github.cbismuth.scala4impatient
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Ch01Ex09_StringOpsSpec extends FlatSpec with Matchers {
+  "first character of a string" should "be extracted with the head function" in {
+    "xyz".head should be('x')
+  }
+
+  "last character of a string" should "be extracted with the last function" in {
+    "xyz".last should be('z')
+  }
+}
