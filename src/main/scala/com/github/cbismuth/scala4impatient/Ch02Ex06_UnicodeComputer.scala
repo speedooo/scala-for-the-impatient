e7ed9c2c6aa89998a8b947c3769464c2c793374a
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-12 17:32:03 +0100

    chapter 02 exercise 06 - unicode product - var name reworked

--- src/main/scala/com/github/cbismuth/scala4impatient/Ch02Ex06_UnicodeComputer.scala
+++ src/main/scala/com/github/cbismuth/scala4impatient/Ch02Ex06_UnicodeComputer.scala
@@ -2,10 +2,10 @@ package com.github.cbismuth.scala4impatient
 
 object Ch02Ex06_UnicodeComputer {
   def unicodeProduct(s: String): Long = {
-    var sum: Long = 1
+    var product: Long = 1
     for (c <- s) {
-      sum *= c.toInt
+      product *= c.toInt
     }
-    sum
+    product
   }
 }
