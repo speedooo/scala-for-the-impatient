e637569e05561da1ef651b6ed56a32dbdf6c3cf5
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-12 21:46:18 +0100

    chapter 02 exercise 08 - unicode product function definition

---
+++ src/main/scala/com/github/cbismuth/scala4impatient/Ch02Ex08_UnicodeComputer.scala
@@ -0,0 +1,9 @@
+package com.github.cbismuth.scala4impatient
+
+import com.github.cbismuth.scala4impatient.Ch02Ex07_UnicodeComputer.unicodeProductWithoutLoop
+
+object Ch02Ex08_UnicodeComputer {
+  def product(s: String): Long = {
+    unicodeProductWithoutLoop(s)
+  }
+}
