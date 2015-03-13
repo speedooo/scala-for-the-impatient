496d8557ef039a2491d3a90ac16beb627c88f928
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-13 14:09:13 +0100

    chapter 03 exercise 01 - random array creator

---
+++ src/main/scala/com/github/cbismuth/scala4impatient/Ch03Ex01_RandomArrayCreatorApp.scala
@@ -0,0 +1,24 @@
+package com.github.cbismuth.scala4impatient
+
+import scala.util.Random
+
+object Ch03Ex01_RandomArrayCreatorApp extends App {
+  printArray(createRandomArrayWithoutYield(10))
+  printArray(createRandomArrayWithYield(10))
+
+  def createRandomArrayWithoutYield(n: Int): Array[Int] = {
+    val a: Array[Int] = new Array[Int](n)
+    var i: Int = 0
+    while (i < a.length) {
+      a(i) = Random.nextInt(n)
+      i += 1
+    }
+    a
+  }
+
+  def createRandomArrayWithYield(n: Int): Array[Int] = {
+    (for (i <- 0 until n) yield Random.nextInt(n)).toArray
+  }
+
+  def printArray(a: Array[Int]) = a foreach (println(_))
+}
