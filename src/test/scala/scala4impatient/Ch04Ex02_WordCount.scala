9ce4c5969221ceb619cda94c44643e086d3e7b74
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-17 17:58:24 +0100

    chapter 04 exercise 02 - word count

---
+++ src/test/scala/scala4impatient/Ch04Ex02_WordCount.scala
@@ -0,0 +1,26 @@
+package scala4impatient
+import org.scalatest.{ FlatSpec, Matchers }
+
+import scala4impatient.Ch04Ex02_WordCount._
+import scala.io.Source
+
+object Ch04Ex02_WordCount {
+  def wc(): Map[String, Int] = {
+    val wc: collection.mutable.Map[String, Int] = collection.mutable.Map[String, Int]() withDefault (_ => 0)
+    Source.fromFile("src/test/scala/scala4impatient/Ch04Ex02_WordCount.txt", "UTF-8")
+      .mkString
+      .toLowerCase
+      .split("\\W+")
+      .foreach(wc(_) += 1)
+    wc.toMap
+  }
+}
+
+class Ch04Ex02_WordCountSpec extends FlatSpec with Matchers {
+  "word count" should "be as below" in {
+    val count: Map[String, Int] = wc()
+    count("fermentum") should be(41)
+    count("tincidunt") should be(102)
+    count("donec") should be(67)
+  }
+}
