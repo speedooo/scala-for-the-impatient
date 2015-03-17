702272d2952f0823f3921379e90b066ce27454ce
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-17 13:50:00 +0100

    chapter 03 exercise 10 - java list to scala buffer conversion

---
+++ src/test/scala/scala4impatient/Ch03Ex10_FlavorSpec.scala
@@ -0,0 +1,34 @@
+package scala4impatient
+
+import java.awt.datatransfer.DataFlavor.imageFlavor
+import java.awt.datatransfer.SystemFlavorMap
+import java.awt.datatransfer.SystemFlavorMap.getDefaultFlavorMap
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.mutable
+import scala4impatient.Ch03Ex10_Flavor._
+
+object Ch03Ex10_Flavor {
+  def nativesForFlavor(): mutable.Buffer[String] = {
+    getDefaultFlavorMap
+      .asInstanceOf[SystemFlavorMap]
+      .getNativesForFlavor(imageFlavor)
+  }
+}
+
+class Ch03Ex10_FlavorSpec extends FlatSpec with Matchers {
+  "native image flavor identifiers" should "be listed below" in {
+    val actual: Array[String] = nativesForFlavor().toArray
+    val expected: Array[String] = Array(
+      "image/jpeg",
+      "image/png",
+      "image/x-png",
+      "image/gif",
+      "PNG",
+      "JFIF"
+    )
+    actual.deep == expected.deep should be(right = true)
+  }
+}
