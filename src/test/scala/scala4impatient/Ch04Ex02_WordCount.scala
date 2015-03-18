ff5b9abfb2c02e5f5f2a4d298ad8d6fbffbcf065
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-18 15:25:25 +0100

    chapter 04 exercise 02 - import cleanup

--- src/test/scala/scala4impatient/Ch04Ex02_WordCount.scala
+++ src/test/scala/scala4impatient/Ch04Ex02_WordCount.scala
@@ -1,12 +1,13 @@
 package scala4impatient
 import org.scalatest.{ FlatSpec, Matchers }
 
+import scala.collection._
 import scala4impatient.Ch04Ex02_WordCount._
 import scala.io.Source
 
 object Ch04Ex02_WordCount {
   def wc(): Map[String, Int] = {
-    val wc: collection.mutable.Map[String, Int] = collection.mutable.Map[String, Int]() withDefault (_ => 0)
+    val wc: mutable.Map[String, Int] = mutable.Map[String, Int]() withDefault (_ => 0)
     Source.fromFile("src/test/scala/scala4impatient/Ch04Ex02_WordCount.txt", "UTF-8")
       .mkString
       .toLowerCase
