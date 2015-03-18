82f91d47b8994c965c1b87d8fa28892d030c1ab5
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-18 16:30:04 +0100

    chapter 04 exercise 03 - word count with "get or else" style

--- src/test/scala/scala4impatient/Ch04Ex03_WordCountWithImmutableMap.scala
+++ src/test/scala/scala4impatient/Ch04Ex03_WordCountWithImmutableMap.scala
@@ -8,13 +8,13 @@ import scala4impatient.Ch04Ex03_WordCountWithImmutableMap._
 import scala.io.Source
 
 object Ch04Ex03_WordCountWithImmutableMap {
-  def wc(): Map[String, Int] = {
-    val words: ArrayBuffer[String] = ArrayBuffer[String]()
-    val count: ArrayBuffer[Int] = ArrayBuffer[Int]()
+  def wcWithZip(): Map[String, Int] = {
     val all: Array[String] = Source.fromFile("src/test/scala/scala4impatient/Ch04Ex02_WordCount.txt", "UTF-8")
       .mkString
       .toLowerCase
       .split("\\W+")
+    val words: ArrayBuffer[String] = ArrayBuffer[String]()
+    val count: ArrayBuffer[Int] = ArrayBuffer[Int]()
     var i: Int = 0
     for (e <- all) {
       if (!words.contains(e)) {
@@ -27,11 +27,29 @@ object Ch04Ex03_WordCountWithImmutableMap {
     }
     words.zip(count).toMap
   }
+
+  def wcWithGetOrElse(): Map[String, Int] = {
+    var wc: immutable.Map[String, Int] = immutable.Map[String, Int]() withDefault (_ => 0)
+    Source.fromFile("src/test/scala/scala4impatient/Ch04Ex02_WordCount.txt", "UTF-8")
+      .mkString
+      .toLowerCase
+      .split("\\W+")
+      .foreach(e => wc = wc + (e -> (wc.getOrElse(e, 0) + 1)))
+    wc
+  }
+
 }
 
 class Ch04Ex03_WordCountWithImmutableMapSpec extends FlatSpec with Matchers {
-  "word count" should "be as below" in {
-    val count: Map[String, Int] = wc()
+  "word count with zip" should "be as below" in {
+    val count: Map[String, Int] = wcWithZip()
+    count("fermentum") should be(41)
+    count("tincidunt") should be(102)
+    count("donec") should be(67)
+  }
+
+  "word count with get or else" should "be as below" in {
+    val count: Map[String, Int] = wcWithGetOrElse()
     count("fermentum") should be(41)
     count("tincidunt") should be(102)
     count("donec") should be(67)
