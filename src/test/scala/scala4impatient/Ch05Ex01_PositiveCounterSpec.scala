a5d8297084506fada6bdeb28b35ad327c7aa02d1
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-19 15:30:13 +0100

    chapter 05 exercise 01 - private var name with underscore prefix

--- src/test/scala/scala4impatient/Ch05Ex01_PositiveCounterSpec.scala
+++ src/test/scala/scala4impatient/Ch05Ex01_PositiveCounterSpec.scala
@@ -3,29 +3,29 @@ package scala4impatient
 import org.scalatest.{ FlatSpec, Matchers }
 
 object Ch05Ex01_PositiveCounter {
-  def apply(init: Int) = {
-    new Ch05Ex01_PositiveCounter(init)
+  def apply(count: Int) = {
+    new Ch05Ex01_PositiveCounter(count)
   }
 }
 
 class Ch05Ex01_PositiveCounter() {
-  private var count: Int = 0
+  private var _count: Int = 0
 
-  def this(init: Int) {
+  def this(count: Int) {
     this()
-    count = init
+    _count = count
   }
 
-  def value: Int = count
+  def count: Int = _count
 
   def increment(): Ch05Ex01_PositiveCounter = {
-    if (count < Int.MaxValue) count += 1
+    if (_count < Int.MaxValue) _count += 1
     this
   }
 }
 
 class Ch05Ex01_PositiveCounterSpec extends FlatSpec with Matchers {
   "positive counter" should "not overflow" in {
-    Ch05Ex01_PositiveCounter(Int.MaxValue).increment().value should be(Int.MaxValue)
+    Ch05Ex01_PositiveCounter(Int.MaxValue).increment().count should be(Int.MaxValue)
   }
 }
