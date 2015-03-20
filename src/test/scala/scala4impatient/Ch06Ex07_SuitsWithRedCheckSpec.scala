ea048fa5581cd0c45e6784900f7831ca8877d158
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-20 18:02:35 +0100

    classname fixes

--- src/test/scala/scala4impatient/Ch06Ex07_SuitsWithRedCheckSpec.scala
+++ src/test/scala/scala4impatient/Ch06Ex07_SuitsWithRedCheckSpec.scala
@@ -2,21 +2,21 @@ package scala4impatient
 
 import org.scalatest.{ FlatSpec, Matchers }
 
-object Ch06Ex07_Suits extends Enumeration {
-  type Ch06Ex07_Suits = Value
+object Ch06Ex07_SuitsWithRedCheck extends Enumeration {
+  type Ch06Ex07_SuitsWithRedCheck = Value
   val HEART = Value("♥")
   val DIAMOND = Value("♦")
   val CLOVER = Value("♣")
   val SPADE = Value("♠")
 
-  def isRed(suits: Ch06Ex07_Suits): Boolean = HEART == suits || DIAMOND == suits
+  def isRed(suits: Ch06Ex07_SuitsWithRedCheck): Boolean = HEART == suits || DIAMOND == suits
 }
 
-class Ch06Ex07_SuitsSpec extends FlatSpec with Matchers {
+class Ch06Ex07_SuitsWithRedCheckWithRedCheckWithRedCheckSpec extends FlatSpec with Matchers {
   "heart and diamond only" should "be red" in {
-    Ch06Ex07_Suits.isRed(Ch06Ex07_Suits.HEART) should be(right = true)
-    Ch06Ex07_Suits.isRed(Ch06Ex07_Suits.DIAMOND) should be(right = true)
-    Ch06Ex07_Suits.isRed(Ch06Ex07_Suits.CLOVER) should be(right = false)
-    Ch06Ex07_Suits.isRed(Ch06Ex07_Suits.SPADE) should be(right = false)
+    Ch06Ex07_SuitsWithRedCheck.isRed(Ch06Ex07_SuitsWithRedCheck.HEART) should be(right = true)
+    Ch06Ex07_SuitsWithRedCheck.isRed(Ch06Ex07_SuitsWithRedCheck.DIAMOND) should be(right = true)
+    Ch06Ex07_SuitsWithRedCheck.isRed(Ch06Ex07_SuitsWithRedCheck.CLOVER) should be(right = false)
+    Ch06Ex07_SuitsWithRedCheck.isRed(Ch06Ex07_SuitsWithRedCheck.SPADE) should be(right = false)
   }
 }
