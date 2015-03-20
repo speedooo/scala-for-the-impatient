702218b0787e41f2c9a6b0d444b7329820ef44d6
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-20 17:28:56 +0100

    chapter 06 exercise 06 - suits

---
+++ src/test/scala/scala4impatient/Ch06Ex06_Suits.scala
@@ -0,0 +1,9 @@
+package scala4impatient
+
+object Ch06Ex06_Suits extends Enumeration {
+  type Ch06Ex06_Suits = Value
+  val HEART = Value("♥")
+  val DIAMOND = Value("♦")
+  val CLOVER = Value("♣")
+  val SPADE = Value("♠")
+}
