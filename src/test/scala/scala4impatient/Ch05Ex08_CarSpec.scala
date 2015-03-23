59d00102161fc88b0f3af604d930f50f3c997f03
Christophe Bismuth <christophe.bismuth@gmail.com>
2015-03-23 21:33:56 +0100

    copyright & scalastyle rules compliance

--- src/test/scala/scala4impatient/Ch05Ex08_CarSpec.scala
+++ src/test/scala/scala4impatient/Ch05Ex08_CarSpec.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package scala4impatient
 
 import org.scalatest.{ FlatSpec, Matchers }
@@ -61,9 +78,7 @@ class Ch05Ex08_CarSpec extends FlatSpec with Matchers {
     val modelYear: Int = 1965
     val licensePlate: String = "123456789"
 
-    var car1: Ch05Ex08_CarWithoutPrivateFields = null
-
-    car1 = new Ch05Ex08_CarWithoutPrivateFields(manufacturer, modelName)
+    var car1: Ch05Ex08_CarWithoutPrivateFields = new Ch05Ex08_CarWithoutPrivateFields(manufacturer, modelName)
     car1.manufacturer should be(manufacturer)
     car1.modelName should be(modelName)
     car1.modelYear should be(-1)
@@ -93,9 +108,7 @@ class Ch05Ex08_CarSpec extends FlatSpec with Matchers {
     car1.licensePlate = "9876543210"
     car1.licensePlate should be("9876543210")
 
-    var car2: Ch05Ex08_CarWithPrivateFields = null
-
-    car2 = new Ch05Ex08_CarWithPrivateFields(manufacturer, modelName)
+    var car2: Ch05Ex08_CarWithPrivateFields = new Ch05Ex08_CarWithPrivateFields(manufacturer, modelName)
     car2.manufacturer should be(manufacturer)
     car2.modelName should be(modelName)
     car2.modelYear should be(-1)
